fd461ebe10a82616ec6117d199c128b27643bb66
kaichen <kaichen@kaichens-MacBook-Pro.local>
2017-06-01 23:20:57 -0400

    job list page

--- build/js/index.js
+++ build/js/index.js
@@ -20,6 +20,24 @@ angular.module('app').config(['$stateProvider', '$urlRouterProvider', function($
 
 angular.module('app').controller('mainCtrl', ['$scope', function($scope){
 
+	$scope.list = [{
+		id: '1',
+		name: '销售',
+		companyName: '千度',
+		imgSrc: 'image/company-3.png',
+		city: '上海',
+		industry: '互联网',
+		time: '2016-06-01 11:05'
+	},{
+		id: '2',
+		name: 'WEB前端',
+		companyName: '木渴望',
+		imgSrc: 'image/company-1.png',
+		city: '北京',
+		industry: '互联网',
+		time: '2016-06-01 11:05'
+	}];
+
 }]);
 'use strict';
 
@@ -37,5 +55,17 @@ angular.module('app').directive('appHead', [function(){
 		restrict: 'A',
 		replace: true,
 		templateUrl: 'view/template/head.html'
+	}
+}]);
+'use strict';
+
+angular.module('app').directive('appPositionList',[function(){   
+	return {
+		restrict: 'A',
+		replace: true,
+		templateUrl: 'view/template/positionList.html',
+		scope:{
+			data: '='
+		}
 	}
 }]);
--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -2,4 +2,22 @@
 
 angular.module('app').controller('mainCtrl', ['$scope', function($scope){
 
+	$scope.list = [{
+		id: '1',
+		name: '销售',
+		companyName: '千度',
+		imgSrc: 'image/company-3.png',
+		city: '上海',
+		industry: '互联网',
+		time: '2016-06-01 11:05'
+	},{
+		id: '2',
+		name: 'WEB前端',
+		companyName: '木渴望',
+		imgSrc: 'image/company-1.png',
+		city: '北京',
+		industry: '互联网',
+		time: '2016-06-01 11:05'
+	}];
+
 }]);
