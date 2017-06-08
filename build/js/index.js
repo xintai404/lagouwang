a94a28293ace37d695d86145820dda2504c74078
xintai404 <kaichen444@gmail.com>
2017-06-07 23:18:25 -0400

    position info

--- build/js/index.js
+++ build/js/index.js
@@ -85,6 +85,21 @@ angular.module('app').directive('appHeadBar', [function(){
 }]);
 'use strict';
 
+angular.module('app').directive('appPositionInfo',[function(){   
+	return {
+		restrict: 'A',
+		replace: true,
+		templateUrl: 'view/template/positionInfo.html',
+		scope:{
+			isActive: '='
+		},
+		link: function(scope){
+			scope.imagePath = scope.isActive? '/image/star-active.png' : '/image/star.png';
+		}
+	}
+}]);
+'use strict';
+
 angular.module('app').directive('appPositionList',[function(){   
 	return {
 		restrict: 'A',
